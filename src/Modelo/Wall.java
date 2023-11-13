1d277fd4086bf5098dbb5b5ef9be25ec15f70153
jmtrovao <joaotrovao.filho@gmail.com>
2023-11-13 01:41:31 -0300

    another brick on the wall
    wall
    wall
    wall
    wall

---
+++ src/Modelo/Wall.java
@@ -0,0 +1,33 @@
+package Modelo;
+
+import Auxiliar.Consts;
+import Auxiliar.Desenho;
+import Controler.ControleDeJogo;
+import Controler.Tela;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import auxiliar.Posicao;
+import Controler.Tela;
+
+public class Wall extends Personagem {
+    
+    public Wall(String sNomeImagePNG) {
+            super(sNomeImagePNG);
+            this.bMortal = false;
+            this.bTransponivel = false;
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
